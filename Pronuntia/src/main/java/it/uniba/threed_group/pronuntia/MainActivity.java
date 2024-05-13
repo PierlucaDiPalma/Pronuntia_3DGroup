27ad0e128544d9ba4aec088c5d279c6b26717305
Luke-cyb <pierlucadipalma@gmail.com>
2024-05-13 11:21:02 +0200

    Start

---
+++ Pronuntia/src/main/java/it/uniba/threed_group/pronuntia/MainActivity.java
@@ -0,0 +1,14 @@
+package it.uniba.threed_group.pronuntia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
